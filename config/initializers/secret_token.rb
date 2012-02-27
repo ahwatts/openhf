53c85da4686d693c78a2cd1cab258461a71c4f35
Andrew Watts <ahwatts@gmail.com>
2012-02-27 16:30:44 -0500

    Initial commit of openhf app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openhf::Application.config.secret_token = '5a5f8418588fa4b945e70c9a8fb8eb2bfd0525d47d148ca8faba87c88c3deeef524d2b6538886e95306f385d9ea6840436ea80584dd6c9ce12973ed7d558bf61'
