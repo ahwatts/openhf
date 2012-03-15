a5704669986a60cf06064a87a1dcae4fec9b8f39
Andrew Watts <ahwatts@gmail.com>
2012-03-15 09:44:57 -0400

    Move the bouncy ball off to HW1.

--- config/routes.rb
+++ config/routes.rb
@@ -56,5 +56,7 @@ Openhf::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  root :to => 'main#index'
+  root :to => 'main#hw1'
+
+  match '/main/:action(.:format)', :controller => 'main', :as => 'main'
 end
